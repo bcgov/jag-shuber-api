5ee1fe3e4a3cf544d50aa104efe03f08300ccd07
Michael Gabelmann <mgabelmann@gmail.com>
2018-03-21 12:11:49 -0700

    CORS filtering
    added a property and class to help with enabling CORS filtering to be less restrictive. Not sure if this is working as I cannot replicate this locally. Will require further testing.

--- openshift/database/liquibase/database.sh
+++ openshift/database/liquibase/database.sh
@@ -12,7 +12,7 @@ export POSTGRES_DATABASE=appdb
 export POSTGRES_DEFAULT_SCHEMA=public
 
 #schema to use for sheriff scheduling application
-export POSTGRES_SCHEMA=testshersched
+export POSTGRES_SCHEMA=shersched
 
 
 java -jar /usr/local/opt/liquibase/libexec/liquibase.jar --driver=org.postgresql.Driver --classpath=/Users/michael.gabelmann/Work/_projects/jag-shuber-api/openshift/java_api/nondeliverable/lib/postgresql-42.2.1.jar --changeLogFile=shersched.db.changelog-master.xml --url="jdbc:postgresql://$POSTGRES_URL:$POSTGRES_PORT/$POSTGRES_DATABASE" --username=$POSTGRES_USER --password=$POSTGRES_PASS --defaultSchemaName=$POSTGRES_DEFAULT_SCHEMA --logLevel=debug update -DPOSTGRES_USER=$POSTGRES_USER -DPOSTGRES_ANON_ROLE=$POSTGRES_ANON_ROLE -DPOSTGRES_SCHEMA=$POSTGRES_SCHEMA
