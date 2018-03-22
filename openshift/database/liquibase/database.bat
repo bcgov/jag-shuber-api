9857c354fa8db99885830a6deab297ac68b8a5f1
Michael Gabelmann <mgabelmann@gmail.com>
2018-03-22 07:02:56 -0700

    Script change
    updated batch script to match shell script for running liquibase, I assume this file will go away in the future.

--- openshift/database/liquibase/database.bat
+++ openshift/database/liquibase/database.bat
@@ -1,21 +1,34 @@
-
-set POSTGRES_USER=shersched
+REM postgres user information
+set POSTGRES_USER=postgres
 set POSTGRES_PASS=password
 
+REM schema to use for liquibase tables (must exist)
+set POSTGRES_DEFAULT_SCHEMA=public
+
+REM postgres connection info and database
 set POSTGRES_URL=localhost
 set POSTGRES_PORT=5432
 set POSTGRES_DATABASE=appdb
+
+REM application user information
+set POSTGRES_APP_USER=shersched
+set POSTGRES_APP_PASS=password
+
+REM catalog and schema to use for sheriff scheduling application
+set POSTGRES_CATALOG=appdb
 set POSTGRES_SCHEMA=shersched
-set POSTGRES_ANON_ROLE=web_anon
 
 liquibase^
- --driver=org.postgresql.Driver^
- --changeLogFile=db.changelog-master.xml^
- --url="jdbc:postgresql://%POSTGRES_URL%:%POSTGRES_PORT%/%POSTGRES_DATABASE%"^
- --username=%POSTGRES_USER%^
- --password=%POSTGRES_PASS%^
- update^
- -DPOSTGRES_USER=%POSTGRES_USER%^
- -DPGRST_DB_SCHEMA=%POSTGRES_SCHEMA%^
- -DPGRST_DB_ANON_ROLE=%POSTGRES_ANON_ROLE%
+    --driver=org.postgresql.Driver^
+    --changeLogFile=shersched.db.changelog-master.xml^
+    --url="jdbc:postgresql://%POSTGRES_URL%:%POSTGRES_PORT%/%POSTGRES_DATABASE%"^
+    --username=%POSTGRES_USER%^
+    --password=%POSTGRES_PASS%^
+    --defaultSchemaName=%POSTGRES_DEFAULT_SCHEMA%^
+    --logLevel=debug^
+    update^
+    -DPOSTGRES_APP_USER=%POSTGRES_APP_USER%^
+    -DPOSTGRES_APP_PASS=%POSTGRES_APP_PASS%^
+    -DPOSTGRES_CATALOG=%POSTGRES_CATALOG%^
+    -DPOSTGRES_SCHEMA=%POSTGRES_SCHEMA%
   
