68a48cb8331120bb0abd78bc8acb76b690f3bad2
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-05-18 07:57:48 -0700

    Just adding some notes around duty import sql

--- src/services/DutyService.ts
+++ src/services/DutyService.ts
@@ -27,9 +27,30 @@ export class DutyService extends DatabaseService<Duty> {
     }
 
     async importDefaults(courthouseId:string, date?:string) : Promise<Duty[]>{
-        const assignmentService = new AssignmentService();
-        const dutyReccurenceService = new DutyRecurrenceService();
-        const dutyService = new DutyService();
+        // const assignmentService = new AssignmentService();
+        // const dutyReccurenceService = new DutyRecurrenceService();
+        
+        // this.squel.select
+
+        // Query for selecting duty recurrences that need duties generated
+        // select 
+        //     dr.duty_recurrence_id, 
+        //     dr.start_time, 
+        //     dr.end_time, 
+        //     dr.sheriffs_required, 
+        //     dr.assignment_id, 
+        //     dr.days_bitmap,
+        //     cast((dr.days_bitmap::bigint) as bit(7)) as days_of_week
+        //     from duty_recurrence as dr join assignment as a 
+        //     on (dr.assignment_id=a.assignment_id)
+        //     where a.courthouse_id='8117ea77-fa37-4442-a865-4e64f70b7cfa'
+        //     and (cast(dr.days_bitmap::bigint as bit(7)) & 8::bit(7))=8::bit(7)
+        //     and NOT EXISTS (
+        //         select duty_id from duty 
+        //         where duty.duty_recurrence_id = dr.duty_recurrence_id
+        //         AND date_trunc('day',duty.start_dtm)=date_trunc('day',current_timestamp)
+        //     )
+
         throw "Not Implemented";
     }
 }
