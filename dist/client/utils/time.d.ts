718b6c72860bc2d87ad3599a090cadbf6542d20d
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-06-01 11:52:16 -0700

    Updating time util to include Date as parameter type

--- dist/client/utils/time.d.ts
+++ dist/client/utils/time.d.ts
@@ -1,4 +1,4 @@
 import { Moment } from "moment";
 export declare const TIME_FORMAT_NO_TZ = "HH:mm:ss";
 export declare const TIME_FORMAT_TZ: string;
-export declare function toTimeString(time: Moment | string | number | undefined): string;
+export declare function toTimeString(time: Moment | string | number | Date | undefined): string;
