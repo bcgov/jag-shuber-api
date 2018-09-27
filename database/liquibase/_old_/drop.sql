4060bf35517578e1bf5280d70e98e59029b4161b
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-09-27 08:16:26 -0700

    Removing the liquibase/_old_ folder

--- database/liquibase/_old_/drop.sql
+++
@@ -1,8 +0,0 @@
--- drop database objects so we can run liquibase again
-
-DROP SCHEMA public CASCADE;
-DROP SCHEMA shersched CASCADE;
-DROP USER shersched;
--- DROP EXTENSION IF EXISTS "uuid-ossp" SCHEMA shersched;
-
-CREATE SCHEMA public;
