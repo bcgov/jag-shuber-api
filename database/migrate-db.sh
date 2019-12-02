653cff510dbae039e064da0d372a5803dac8451c
Tiago Graf <tiago.graf@gmail.com>
2019-12-02 12:10:36 -0800

    Add escort assignments and uncomment liquibase script

--- database/migrate-db.sh
+++ database/migrate-db.sh
@@ -8,20 +8,19 @@ export PG_ADMIN_USER="postgres"
 
 cd "${SCRIPTDIR}/liquibase/"
 
-echo "Skipping Liquibase"
+liquibase --driver=org.postgresql.Driver \
+         --contexts="$LIQUIBASE_CONTEXTS" \
+         --changeLogFile=shersched.db.changelog.MASTER.xml \
+         --url="jdbc:postgresql://postgres:$POSTGRES_SERVICE_PORT/$PG_DATABASE" \
+         --username=$PG_ADMIN_USER \
+         --password=$PG_ADMIN_PASSWORD \
+         --defaultSchemaName=$PG_DEFAULT_SCHEMA \
+         --logLevel=info update \
+         -DPOSTGRES_APP_USER=$PG_USER \
+         -DPOSTGRES_APP_PASS=$PG_PASSWORD \
+         -DPOSTGRES_CATALOG=$PG_DATABASE \
+         -DPOSTGRES_SCHEMA=$API_DATABASE_SCHEMA \
+         -DPOSTGRES_EXT_SCHEMA=$POSTGRES_EXT_SCHEMA
 
-#liquibase --driver=org.postgresql.Driver \
-#          --contexts="$LIQUIBASE_CONTEXTS" \
-#          --changeLogFile=shersched.db.changelog.MASTER.xml \
-#          --url="jdbc:postgresql://postgres:$POSTGRES_SERVICE_PORT/$PG_DATABASE" \
-#          --username=$PG_ADMIN_USER \
-#          --password=$PG_ADMIN_PASSWORD \
-#          --defaultSchemaName=$PG_DEFAULT_SCHEMA \
-#          --logLevel=info update \
-#          -DPOSTGRES_APP_USER=$PG_USER \
-#          -DPOSTGRES_APP_PASS=$PG_PASSWORD \
-#          -DPOSTGRES_CATALOG=$PG_DATABASE \
-#          -DPOSTGRES_SCHEMA=$API_DATABASE_SCHEMA \
-#          -DPOSTGRES_EXT_SCHEMA=$POSTGRES_EXT_SCHEMA
-
+echo "Finished Liquibase"
 
