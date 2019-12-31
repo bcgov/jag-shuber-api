95cc715c2624634a49be0f255b8ca7bb7aecb686
Lucas <tech@firebrandwebsolutions.com>
2019-12-31 14:44:17 -0800

    Add frontend permissions map to user token.

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -2,16 +2,24 @@ import { createToken } from '../infrastructure/token';
 import { TokenPayload } from '../common/authentication';
 import { AutoWired, Container } from 'typescript-ioc';
 
+import { ApiScopeService } from './ApiScopeService';
+import { FrontendScopeService } from './FrontendScopeService';
+import { FrontendScopePermissionService } from '../services/FrontendScopePermissionService';
 import { UserService } from '../services/UserService';
 import { UserRoleService } from '../services/UserRoleService';
 import { RoleService } from '../services/RoleService';
+import { RolePermissionService } from './RolePermissionService';
 
 import { Scope, Scopes } from '../common/authentication'
 
 import { UserRole } from '../models/UserRole';
 import { RoleFrontendScope } from '../models/RoleFrontendScope';
 import { RoleApiScope } from '../models/RoleApiScope';
-import { ApiScopeService } from './ApiScopeService';
+import { FrontendScope } from '../models/FrontendScope';
+import { ApiScope } from '../models/ApiScope';
+import { RolePermission } from '../models/RolePermission';
+import { FrontendScopePermission } from '../models/FrontendScopePermission';
+import { RoleFrontendScopePermission } from '../models/RoleFrontendScopePermission';
 
 const TEST_USER_ID = 'd9772aab-6e5e-4b41-87b2-3294009e6d28';
 
@@ -49,6 +57,7 @@ export class TokenService {
         if (!tokenPayload || (tokenPayload.userId !== TEST_USER_ID)) {
             console.log(`Test User [${TEST_USER_ID}] OAuth scopes`);
             console.log(authScopes);
+            console.log(appScopes);
         }
 
         const token = await createToken({
@@ -63,42 +72,6 @@ export class TokenService {
         // console.log(`User [${userId}] token`);
         // console.log(token);
         return token;
-    }
-
-    private async buildUserAppScopes(userId: string): Promise<any> {
-        const userRoleService = Container.get(UserRoleService);
-        const userRoles = await userRoleService.getByUserId(userId);
-
-        const roleService = Container.get(RoleService);
-        
-        const scopes = await userRoles.reduce(async (userScopeCodes: Promise<string[]>, ur: UserRole) => {
-            let results = await userScopeCodes;
-            // For each UserRole, attach the role
-            if (ur && ur.roleId) {
-                const role = await roleService.getById(ur.roleId);
-                // RoleFrontendScopes and RoleApiScopes will be populated
-                // Let's loop over the RoleApiScopes
-                ur.role = role;
-                const roleScopeCodes: string[] = (ur.role && ur.role.roleFrontendScopes) 
-                    ? ur.role.roleFrontendScopes
-                        .reduce((scopeCodes: string[], cur: RoleFrontendScope) => {
-                            if (cur.scope && cur.scope.scopeCode) {
-                                scopeCodes.push(cur.scope.scopeCode as string);
-                            }
-                            return scopeCodes;
-                        }, [])
-
-                    : [];
-
-                if (roleScopeCodes.length > 0) {
-                    return results.concat(roleScopeCodes as string[]);
-                }
-            }
-
-            return userScopeCodes;
-        }, Promise.resolve(['DEFAULT'] as string[]));
-
-        return scopes;
     }
 
     private async buildUserAuthScopes(userId: string): Promise<Scope[]> {
@@ -135,5 +108,85 @@ export class TokenService {
         }, Promise.resolve(['default'] as Scope[]));
 
         return scopes;
+    }
+
+    private async buildUserAppScopes(userId: string): Promise<any> {
+        const userRoleService = Container.get(UserRoleService);
+        const userRoles = await userRoleService.getByUserId(userId);
+
+        const roleService = Container.get(RoleService);
+        
+        const scopes = await userRoles.reduce(async (asyncUserScopeCodes: Promise<{ [key: string]: string }[]>, ur: UserRole) => {
+            let userScopeCodes = await asyncUserScopeCodes;
+            // For each UserRole, attach the role
+            if (ur && ur.roleId) {
+                const role = await roleService.getById(ur.roleId);
+                ur.role = role;
+                const roleScopes: { [key: string]: string }[] = (ur.role && ur.role.roleFrontendScopes) 
+                    ?  await ur.role.roleFrontendScopes
+                        .reduce(async (asyncScopeCodes: Promise<string[]>, cur: RoleFrontendScope) => {
+                            let scopeCodes = await asyncScopeCodes;
+
+                            if (cur.scope && cur.scope.scopeCode) {
+                                const permissions = await this.buildRoleFrontendScopePermissions(cur, cur.scope)
+                                scopeCodes[cur.scope.scopeCode as string] = (permissions.length > 0) ? permissions : true;
+                            }
+                            return scopeCodes;
+                        }, Promise.resolve([]))
+                    : [];
+
+                if (Object.keys(roleScopes).length > 0) {
+                    return Object.assign({}, userScopeCodes, roleScopes);
+                }
+            }
+
+            return userScopeCodes;
+        }, Promise.resolve({}));
+
+        // console.log(scopes);
+        return scopes;
+    }
+
+    
+    /** 
+     * Build out the permissions configuration for a given role scope.
+     */
+    private async buildRoleFrontendScopePermissions(roleFrontendScope: RoleFrontendScope, currentScope: FrontendScope): Promise<any[]> {
+        const scopePermissionService = Container.get(FrontendScopePermissionService);
+        const rolePermissionService = Container.get(RolePermissionService);
+    
+        const scopePermissions = await scopePermissionService.getByScopeId(currentScope.id);
+        const assignedPermissions = await rolePermissionService.getByRoleFrontendScopeId(roleFrontendScope.id);
+
+        return scopePermissions.map((fsp: FrontendScopePermission): RoleFrontendScopePermission => {
+            // RolePermission doesn't have a frontendScopeId or a scopeId reference directly on it.
+            // This is by design.
+            // We basically just apply a RolePermission - if it exists - on top of a RoleFrontendScopePermission,
+            // which combines the FrontendScopePermission and RolePermission shapes, and which is also the actual
+            // model used by the UI in redux-form FormArrays.
+            let roleScopePermission = assignedPermissions.find((ap) => ap.frontendScopePermissionId === fsp.id) as RoleFrontendScopePermission;
+
+            let hasPermission = false;
+            if (roleScopePermission && roleScopePermission.id) {
+                hasPermission = true;
+            } else {
+                roleScopePermission = {} as RoleFrontendScopePermission;
+            }
+
+            roleScopePermission.roleId = roleFrontendScope.roleId;
+            roleScopePermission.roleFrontendScopeId = roleFrontendScope.id;
+            roleScopePermission.frontendScopePermissionId = fsp.id;
+            roleScopePermission.displayName = fsp.displayName;
+            roleScopePermission.description = fsp.description;
+            roleScopePermission.hasPermission = hasPermission;
+            // roleScopePermission.scope = currentScope;
+            // roleScopePermission.roleScope = roleFrontendScope;
+            // roleScopePermission.scopePermission = fsp;
+
+            return {
+                ...roleScopePermission,
+                ...fsp
+            };
+        });
     }
 }
