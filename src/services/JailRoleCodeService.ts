7b2c11ba2383baf55cd31aa7c6bfce26fca029e3
Lucas <tech@firebrandwebsolutions.com>
2020-01-23 19:24:31 -0800

    Override getInsertQuery for CourtRoleCodeService, JailRoleCodeService and OtherAssignCodeService - they all use code as their PKs.

--- src/services/JailRoleCodeService.ts
+++ src/services/JailRoleCodeService.ts
@@ -1,6 +1,7 @@
 import { JailRoleCode } from '../models/JailRoleCode';
 import ExpirableDatabaseService from '../infrastructure/ExpirableDatabaseService';
 import { AutoWired } from 'typescript-ioc';
+import { PostgresInsert } from 'squel';
 
 @AutoWired
 export class JailRoleCodeService extends ExpirableDatabaseService<JailRoleCode> {
@@ -12,6 +13,23 @@ export class JailRoleCodeService extends ExpirableDatabaseService<JailRoleCode> 
 
     constructor() {
         super('jail_role_code', 'jail_role_code');
+    }
+
+    protected getInsertQuery(entity: Partial<JailRoleCode>): PostgresInsert {
+        const query = this.db.insertQuery(this.tableName) // Ignore the PK, we don't use a UUID here
+            .returning(this.getReturningFields());
+        this.setQueryFields(query, entity, true);
+
+        // Take the Field Map keys and map properties from the object
+        const objectPropName = this.fieldMap[this.effectiveField];
+        const propValue = entity[objectPropName];
+
+        if (!propValue) {
+            // If the effective date field was not provided
+            // Set it to NOW
+            query.set(this.effectiveField, this.squel.str('NOW()'));
+        }
+        return query;
     }
 
 }
--- src/services/OtherAssignCodeService.ts
+++ src/services/OtherAssignCodeService.ts
@@ -1,6 +1,7 @@
 import { OtherAssignCode } from '../models/OtherAssignCode';
 import ExpirableDatabaseService from '../infrastructure/ExpirableDatabaseService';
 import { AutoWired } from 'typescript-ioc';
+import { PostgresInsert } from 'squel';
 
 @AutoWired
 export class OtherAssignCodeService extends ExpirableDatabaseService<OtherAssignCode> {
@@ -12,6 +13,23 @@ export class OtherAssignCodeService extends ExpirableDatabaseService<OtherAssign
 
     constructor() {
         super('other_assign_code', 'other_assign_code');
+    }
+
+    protected getInsertQuery(entity: Partial<OtherAssignCode>): PostgresInsert {
+        const query = this.db.insertQuery(this.tableName) // Ignore the PK, we don't use a UUID here
+            .returning(this.getReturningFields());
+        this.setQueryFields(query, entity, true);
+
+        // Take the Field Map keys and map properties from the object
+        const objectPropName = this.fieldMap[this.effectiveField];
+        const propValue = entity[objectPropName];
+
+        if (!propValue) {
+            // If the effective date field was not provided
+            // Set it to NOW
+            query.set(this.effectiveField, this.squel.str('NOW()'));
+        }
+        return query;
     }
 
 }
