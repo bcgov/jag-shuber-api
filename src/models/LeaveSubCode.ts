4447ec2b69eb154b0a73308fcf6e0d0b27df7693
Lucas <tech@firebrandwebsolutions.com>
2020-03-23 12:54:29 -0700

    Update leave types model and service, add sort_order and normalize columns.

--- src/models/LeaveSubCode.ts
+++ src/models/LeaveSubCode.ts
@@ -1,6 +1,13 @@
 export interface LeaveSubCode {
     code: string;
     subCode: string;
-    description: string;
+    description?: string; // TODO: For future use
+    effectiveDate?: string;
     expiryDate?: string;
+    sortOrder?: number;
+    createdBy?: string;
+    updatedBy?: string;
+    createdDtm?: string;
+    updatedDtm?: string;
+    revisionCount?: number;
 }
