f3f97c6204c935715edc56add810b0f034c1c67b
Lucas <tech@firebrandwebsolutions.com>
2020-05-01 11:18:02 -0700

    Update common types.

--- dist/common/types.d.ts
+++ dist/common/types.d.ts
@@ -1,6 +1,6 @@
-import { Moment } from 'moment';
-export declare type DateType = string | Date | Moment | number;
-export declare type TimeType = string | number | Moment;
+import moment from 'moment';
+export declare type DateType = string | Date | moment.Moment | number;
+export declare type TimeType = string | number | moment.Moment;
 export declare type TimeRange = {
     startTime: TimeType;
     endTime: TimeType;
